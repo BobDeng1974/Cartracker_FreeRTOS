f04f4d6a33392e8a129a1a59623b968231f4cd0c
Sami Hamici <sami.hamici@corefactory.com>
2019-08-26 23:25:38 +0300

    Added references for GPS module

---
+++ README.md
@@ -0,0 +1,8 @@
+# Car Tracker embedded software
+
+### Reference
+
+u-blox NEO-6M-0-001
+1. https://www.u-blox.com/sites/default/files/products/documents/NEO-6_DataSheet_%28GPS.G6-HW-09005%29.pdf
+2. https://www.terraelectronica.ru/pdf/show?pdf_file=%2Fz%2FDatasheet%2FU%2FUART+GPS+NEO-6M+User+Manual.pdf
+3. https://www.u-blox.com/sites/default/files/products/documents/u-blox6_ReceiverDescrProtSpec_%28GPS.G6-SW-10018%29_Public.pdf?utm_source=en%2Fimages%2Fdownloads%2FProduct_Docs%2Fu-blox6_ReceiverDescriptionProtocolSpec_%28GPS.G6-SW-10018%29.pdf
--- inc/GNSSHandler.h
+++ inc/GNSSHandler.h
@@ -11,6 +11,7 @@
 #ifndef GNSSHANDLER_H_
 #define GNSSHANDLER_H_
 
+#define GNSS_BUFFER_SIZE 100
 /* NEO6MV2
  * Default baud 9600
  *
@@ -23,57 +24,14 @@ public:
 	virtual ~GNSSHandler();
 	void read();
 	void configure();
-	// Informs if filtered message is available from GNSS
-	void parsedMessageAvailable();
-	void process();
 
 private:
-	// TODO this is not very memory saving favorable, see if this can be more compact.
-	// Contains latest date data
-	char dateg[15];
-	// Contains latest time data
-	char timeg[15];
-	// Contains latest latitude
-	char Latitude[15];
-	// Contains latest latitude in float type
-	double Latitude_f;
-	// Contains latest latitude direction
-	char Latitude_direction[15];
-	// Contains latest longitude data
-	char Longitude[15];
-	// Contains latest longitude as float
-	double Longitude_f;
-	// Contains latest longitude direction
-	char Longitude_direction[15];
-	// Contains amoun of satellites
-	char Satellites[15];
-	// Contains latest altitude data
-	char Altitude[15];
-	// Checks if full message received, if yes parse message correctly and set gnss message flag high
-	void parseMessage();
-	// Set static buffer, limited amount of memory. Dynamic could exceed heap.
-	static char localBuffer[100];
-	int localBufPointer;
-	// read line from buffer
-	void getline();
-	// Extract meaningful data from received GNSS message
-	void data_extractor(); // 33-40 comma locations
-	// Find data locations
-	void data_positions();
-	// Receive data from GNSS module
-	void receive_data();
 
-	// Print time
-	void time();
-	// Print date
-	void date();
-	// Print coordinates
-	void cordinates();
-	// Print satellites
-	void satellites();
-	// Print all data
-	void printdata();
-	void float_conversion();
+	// Checks if full message received, if yes parse message correctly and set gnss message flag high
+	void parseMessage(void);
+	// Set static buffer, limited amount of memory. Dynamic could exceed heap.
+	static char localBuffer[GNSS_BUFFER_SIZE];
+	int localBufPointer;
 
 };
 
